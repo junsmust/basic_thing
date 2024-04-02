3a5cc1b7202f90f6ebfd9b2e94398a94736f7380
임주영 <isp3087@gmail.com>
2024-04-03 00:39:19 +0900

    클래스와 구조체에 대해서

---
+++ Basic_Thing/Basic_Thing/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Basic_Thing
+//
+//  Created by 임주영 on 2024/04/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
